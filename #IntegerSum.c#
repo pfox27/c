87a6e2b1b8a9e4a932dad54d80db80b57f194c0e
Paul <pfox27@gmail.com>
2023-03-06 19:03:54 -0500

    update

---
+++ #IntegerSum.c#
@@ -0,0 +1,20 @@
+/* Example 4.3 - A program that computes the sum from 1 to a user-specified number. */
+
+#include<stdio.h>
+
+int main(){
+  long sum = 0L;  /* Stores the sum of the integers */
+  int count = 0;  /* The number of integers to be summed. */
+  int i = 0; /* The loop counter */
+
+  printf("\nPlease enter the number of integers you'd like summed:\n"); /* Read the number of integers to be summed */
+  scanf(" %d", &count);
+
+  /* Sum integers from 1 to count. */
+
+  for(i = 1; i <= count; ++i)
+    sum += i;
+
+    printf("\nThe sum of the first %d numbers is %ld\n", count, sum);
+  }
+  
--- Factorial.c
+++ Factorial.c
@@ -3,24 +3,22 @@
 #include<stdio.h>
 
 int main(){
-  long factorial = 0L;  /* Stores the value of the factorial. */
-  int integer = 0;  /* The integer for which the factorial should be computed. */
+  int factorial = 0;  /* Stores the value of the factorial. */
+  int number = 0;  /* The integer for which the factorial should be computed. */
   int i = 0; /* The loop counter. */
 
   printf("\nPlease enter the integer for which you'd like the factorial computed:\n"); /* Read the integer for which the factorial must be computed*/
-  scanf(" %d", &integer);
+  scanf(" %d", &number);
 
   /* Compute the factorial for the integer input. */
 
-  factorial = integer;
-  printf("/nThe value of factorial is currently %ld\n", factorial);
-  printf("/nThe value of i is currently %d\n", i);
+  factorial = number;
   
-  for(i = integer; i <= 2; i--){
-    factorial = factorial * (i - 1);
-  printf("/nThe value of %ld is currently\n", factorial);
-  printf("/nThe value of %d is currently\n", i);
-}
-    printf("\nThe factorial of the number %d is %ld\n", integer, factorial);
+  for(i = number; i >= 2; i--)
+    factorial = factorial * (i - 1); 
+
+    printf("\nThe factorial of the number %d is %d\n", number, factorial);
+    printf("\n");
+    return 0;
   }
   
