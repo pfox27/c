01a58329ef1345dfc6d0c800538de7a6c94968e6
Paul <pfox27@gmail.com>
2023-07-11 18:14:11 -0400

    update

---
+++ CubeVolume.c
@@ -0,0 +1,31 @@
+/* A programme that computes the volume of a cube using a function and a given length, width and height */
+
+#include <stdio.h>
+
+float cube_volume(float length, float width, float height);
+
+int main(){
+	long volume;
+	volume = cube_volume(200, 50, 10);
+	printf("\nThe volume of the cube is %ld", volume);
+	printf(" L\n");
+}
+
+/*********************************************************************
+ * cube_volume()
+ * Function determines the volume of a cube
+ * Argument list --> 	float length:  length of cube
+ * 			float width: width of cube
+ * 			float height: height of cube
+ * Return value -->	float 		volume of cube
+ */
+
+float cube_volume(float length, float width, float height){
+	return (length * width * height);
+}
+
+
+
+
+
+
